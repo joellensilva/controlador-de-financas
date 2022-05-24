5136c92a75dea9379d69f60e3b039f12bc151bfc
Joellen Silva <99025532+Joellensilva@users.noreply.github.com>
2022-05-24 11:27:27 -0300

    Importei os arquivos do AndroidStudio

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:controlador_de_financas/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomePage()
+  ),
+  );
+}
+
