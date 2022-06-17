7415d923097fbb969e1dc7470ccc81a61fd6cf38
Joellensilva <joellen.absilva@gmail.com>
2022-06-17 16:25:58 -0300

    Quinta e sexta telas

--- flutter_application_1/lib/quinta_page.dart
+++ flutter_application_1/lib/quinta_page.dart
@@ -22,21 +22,295 @@ class _QuintaPageState extends State<QuintaPage> {
       backgroundColor: Color(0xFF1E5234),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-             const Text(
-                'Tops destinos mais buscados',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Color(0xFF1E5234),
-                      fontWeight: FontWeight.bold,
-                      backgroundColor: Colors.white,
+              child: ListView(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                const Text(
+                                  'Nota',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Text(
+                                  'Data de vencimento:',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                const Text(
+                                  'R\$****',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Text(
+                                  '00/00/0000',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                        ],
+                      ),
                     ),
+                     const SizedBox(height: 8),
+                     Container(
+                      decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                const Text(
+                                  'Nota',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Text(
+                                  'Data de vencimento:',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                const Text(
+                                  'R\$****',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Text(
+                                  '00/00/0000',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                     Container(
+                      decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                const Text(
+                                  'Nota',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Text(
+                                  'Data de vencimento:',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                const Text(
+                                  'R\$****',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Text(
+                                  '00/00/0000',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                     Container(
+                      decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                const Text(
+                                  'Nota',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Text(
+                                  'Data de vencimento:',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                const Text(
+                                  'R\$****',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Text(
+                                  '00/00/0000',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                     Container(
+                      decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                const Text(
+                                  'Nota',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Text(
+                                  'Data de vencimento:',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                const Text(
+                                  'R\$****',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Text(
+                                  '00/00/0000',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF1E5234),
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            
+                        ],
+                      ),
+                    ),
+                  ],
               ),
-          ],
-        ), 
-        ),
-    );
+            ),
+        );
   }
 
 }
