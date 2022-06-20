b75a98021d3d41d25d8ed4e637dcc3331e0a331d
jhonathanSC <jsc6@aluno.ifal.edu.br>
2022-06-20 12:07:09 -0300

    Primeira e segunda telas

--- flutter_application_1/lib/segunda_page.dart
+++ flutter_application_1/lib/segunda_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import CustomIcons from '../lib/presentation/custom_icons_icons.dart'
 
 class SegundaPage extends StatefulWidget {
   const SegundaPage({ Key? key }) : super(key: key);
@@ -13,54 +12,81 @@ class _SegundaPageState extends State<SegundaPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+       appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color(0xFF1A422B),
+        title: Text(
+          'Control\$',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
       backgroundColor: Color(0xFF1E5234),
       body: Padding (
         padding: const EdgeInsets.all(16.0),
         child: Center(
-          //crossAxisAlignment: CrossAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.spaceEvenly,
           child:
             Column (
-              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-              const Text( 
-                '\$',
-                    style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    )
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(8),
-                       ),
-                       padding: const EdgeInsets.all(8),
-                       child: Row(children: [
-                       Tab(icon: Icon(CustomIcons.lock_outline)),
-                       ]),
-                    )
-               const Text(
-                '@',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white
-                 ),
+                const SizedBox(height: 80),
+                    Icon(
+          Icons.attach_money ,
+          size: 100,
+          color: Colors.white,
+        ),
+        TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Usuário:',
+                
+                filled: true,
+                fillColor: Colors.white,
               ),
-               const Text(
-                'a',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white
-                 ),
+            ),  
+            const SizedBox(height: 24),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Senha:',
+                
+                filled: true,
+                fillColor: Colors.white,
               ),
-              const Text(
-                'Entrar',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white
-                 ),
-              ),
+            ),  
+       /* Icon(
+          Icons.alternate_email,
+          size: 50,
+          color: Color(0xFF1A422B),
+        ),
+        Icon(
+          Icons.lock_outline_sharp,
+          size: 50,
+          color: Color(0xFF1A422B),
+        ),*/
+        
+            const SizedBox(height: 24),
+       ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return const SegundaPage();
+                                },
+                              ),);
+                          },
+                          child: const Text(
+                            'Entrar',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary:  Color(0xFF1A422B),
+                          ),
+                        )
               ],
              ),
              ),
