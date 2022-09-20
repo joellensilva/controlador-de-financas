9b6e355d08293b5460f84477d002a5d5b737b3cb
Joellensilva <joellean.absilva@gmail.com>
2022-09-20 11:33:14 -0300

    tatata

--- flutter_application_1/lib/segunda_page.dart
+++ flutter_application_1/lib/segunda_page.dart
@@ -8,14 +8,43 @@ import 'package:flutter_application_1/terceira_page.dart';
 import 'home_page.dart';
 
 class SegundaPage extends StatefulWidget {
-  const SegundaPage({Key? key}) : super(key: key);
-
+  const SegundaPage({Key? key, required this.onSubmit}) : super(key: key);
+  final ValueChanged<String> onSubmit;
   @override
   State<SegundaPage> createState() => _SegundaPageState();
 }
 
 class _SegundaPageState extends State<SegundaPage> {
+  final _usuario = TextEditingController();
+  final _senha = TextEditingController();
   @override
+  void dispose() {
+    _usuario.dispose();
+    _senha.dispose();
+    super.dispose();
+  }
+  String? get _errorText {
+  // at any time, we can get the text from _controller.value.text
+  final user = _usuario.value.text;
+  final pass = _senha.value.text;
+  // Note: you can do your own custom validation here
+  // Move this logic this outside the widget for more testable code
+  if (user.isEmpty) {
+    return 'Can\'t be empty';
+  }
+  if (pass.isEmpty) {
+    return 'Can\'t be empty';
+  }
+  // return null if the text is valid
+  return null;
+}
+void _submit() {
+  // if there is no error text
+  if (_errorText == null) {
+    // notify the parent widget via the onSubmit callback
+    widget.onSubmit(_usuario.value.text);
+  }
+}
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -42,21 +71,26 @@ class _SegundaPageState extends State<SegundaPage> {
                     size: 100,
                     color: Colors.white,
                   ),
+                  
                   TextField(
+                    controller: _usuario,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: 'Usuário:',
                       filled: true,
                       fillColor: Colors.white,
+                      errorText: _errorText,
                     ),
                   ),
                   const SizedBox(height: 24),
                   TextField(
+                    controller: _senha,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: 'Senha:',
                       filled: true,
                       fillColor: Colors.white,
+                      errorText: _errorText,
                     ),
                   ),
                   /* Icon(
@@ -81,6 +115,9 @@ class _SegundaPageState extends State<SegundaPage> {
                     ),
                   );
                 },
+                _usuario.value.text.isNotEmpty
+      ? _submit
+      : null,
                 child: const Text(
                   'Entrar',
                   style: TextStyle(
@@ -148,3 +185,4 @@ class _SegundaPageState extends State<SegundaPage> {
     );
   }
 }
+
