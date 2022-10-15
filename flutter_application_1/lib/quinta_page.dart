b4bd45ad2e9d06f68a856182a19513489ec8214d
Joellensilva <joellen.absilva@gmail.com>
2022-10-15 16:35:21 -0300

    editando

--- flutter_application_1/lib/quinta_page.dart
+++ flutter_application_1/lib/quinta_page.dart
@@ -42,4 +42,3 @@ class _QuintaPageState extends State<QuintaPage> {
     );
   }
 }
-
--- flutter_application_1/lib/sexta_page.dart
+++ flutter_application_1/lib/sexta_page.dart
@@ -14,102 +14,105 @@ class _SextaPageState extends State<SextaPage> {
       backgroundColor: Color(0xFF1E5234),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            Center (
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: 32.0),
-            Text(
-              'Você gastou',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-              fontSize: 32,
-              color: Colors.white,
-              fontWeight: FontWeight.w300,
+        child: ListView(children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.5,
+            width: MediaQuery.of(context).size.width * 0.3,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 32.0),
+                  Text(
+                    'Você gastou',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.height * 0.032,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  Text(
+                    'R\$*****',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.height * 0.04,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'de R\$*****',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.height * 0.024,
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Sua meta de reserva é',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.height * 0.03,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  Text(
+                    'R\$*****',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.height * 0.04,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Isso significa que você tem',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.height * 0.03,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  Text(
+                    'R\$ *****',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.height * 0.04,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Disponível',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.height * 0.024,
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Você está indo muito bem.\nParabéns!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.height * 0.03,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            ),
-            Text(
-              'R\$*****',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-              fontSize: 40,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-            Text(
-              'de R\$*****',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-              fontSize: 24,
-              color: Colors.white,
-              fontWeight: FontWeight.normal,
-            ),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Sua meta de reserva é',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-              fontWeight: FontWeight.w300,
-            ),
-            ),
-            Text(
-              'R\$*****',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-              fontSize: 40,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Isso significa que você tem',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-              fontWeight: FontWeight.w300,
-            ),
-            ),
-            Text(
-              'R\$ *****',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-              fontSize: 40,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-            Text(
-              'Disponível',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-              fontSize: 24,
-              color: Colors.white,
-              fontWeight: FontWeight.normal,
-            ),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Você está indo muito bem.\nParabéns!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-          ],
-        ),
-        ),])
-    ),
-
+          ),
+        ]),
+      ),
     );
   }
 }
