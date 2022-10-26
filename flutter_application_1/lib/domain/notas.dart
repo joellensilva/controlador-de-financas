0c2610f105814ba73fd54fb3c8d7174b712d7e7d
Gabriel <gabriel.almeida.12.biel@gmail.com>
2022-10-26 14:47:13 +0000

    preparando o merge

--- flutter_application_1/lib/domain/notas.dart
+++ flutter_application_1/lib/domain/notas.dart
@@ -1,26 +1,9 @@
-/*
 import 'dart:ffi';
 
 class Notas {
-  late final String localDivida;
-  late final String data;
-  late final double valor;
+  String localDivida;
+  String data;
+  double valor;
 
   Notas({required this.localDivida, required this.data, required this.valor});
-
-  Map<String, Object?> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-
-    data['localDivida'] = localDivida;
-    data['data'] = data;
-    data['valor'] = valor;
-    return data;
-  }
-
-  Notas.fromJson(Map<String, dynamic> json) {
-    localDivida = json['localDivida'];
-    data = json['data'];
-    valor = json['valor'];
-  }
 }
-*/
