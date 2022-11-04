e27df8dd8be83155ed37477e23038c72eb46b491
Gabriel <103127611+gbrieldas@users.noreply.github.com>
2022-11-04 10:31:19 -0300

    Update terceira_page.dart
    organizando a terceira página

--- flutter_application_1/lib/pages/terceira_page.dart
+++ flutter_application_1/lib/pages/terceira_page.dart
@@ -3,9 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/data/usuario_dao.dart';
 import 'package:flutter_application_1/domain/usuario.dart';
-// import 'package:flutter_application_1/data/usuario_dao.dart';
-// import 'package:flutter_application_1/domain/usuario.dart';
-
 
 class TerceiraPage extends StatefulWidget {
   const TerceiraPage({Key? key}) : super(key: key);
@@ -18,12 +15,13 @@ class _TerceiraPageState extends State<TerceiraPage> {
   int selectedIndex = 0;
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    backgroundColor: Color(0xFF1E5234),
-    body: buildFutureBuilder()
-  );
-}
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1E5234),
+      body: buildFutureBuilder()
+    );
+  }
+
   buildFutureBuilder() {
     Future<List<Usuario>> futureLista = UsuarioDao().listarUsuarios();
 
@@ -32,7 +30,6 @@ Widget build(BuildContext context) {
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           List<Usuario> lista = snapshot.data ?? [];
-
           return buildColumn(lista, context);
         }
         return const Center(child: CircularProgressIndicator());
@@ -40,52 +37,69 @@ Widget build(BuildContext context) {
     );
   }
 }
+
 buildColumn(List<Usuario> lista, context) {
-      return ListView(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: Text(
-                  "Control\$ com você!",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 1),
-                child: Card(
-                  elevation: 0,
-                  color: Colors.white,
-                  child: SizedBox(
-                    width: 1000,
-                    height: 50,
-                    child: Center(child: Text('Nome: ${lista[0].nome}')),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 1),
-                child: Card(
-                  elevation: 0,
-                  color: Colors.white,
-                  child: SizedBox(
-                    width: 1000,
-                    height: 50,
-                    child: Center(child: Text('Renda Mensal:')),
-                  ),
-                ),
-              ),
 
-            ],
+  return ListView(
+    children: [
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: Text(
+              "Control\$ com você!",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        ],
-      );
-  }
 
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 1),
+            child: Card(
+              elevation: 0,
+              color: Colors.white,
+              child: SizedBox(
+                width: 1000,
+                height: 50,
+                child: Center(child: Text('${lista[0].nome}')),
+              ),
+            ),
+          ),
+
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 1),
+            child: Card(
+              elevation: 0,
+              color: Colors.white,
+              child: SizedBox(
+                width: 1000,
+                height: 50,
+                child: Center(child: Text('Renda Mensal:')),
+              ),
+            ),
+          ),
+
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 1),
+            child: Card(
+              elevation: 0,
+              color: Colors.white,
+              child: SizedBox(
+                width: 1000,
+                height: 50,
+                child: Center(child: Text('Meta de Reserva:')),
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    ],
+  );
+}
