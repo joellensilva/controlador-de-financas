bfe3952dbf016f03871d6c8891d7965e5b3f5df5
Gabriel <gabriel.almeida.12.biel@gmail.com>
2022-12-27 12:43:46 +0000

    update

--- flutter_application_1/lib/pages/quarta_page.dart
+++ flutter_application_1/lib/pages/quarta_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/terceira_page.dart';
 import 'package:flutter_application_1/data/divida_dao.dart';
@@ -22,137 +24,12 @@ class _QuartaPageState extends State<QuartaPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SafeArea(
+      child: Scaffold(
         backgroundColor: Color(0xFF1E5234),
         body: buildBody(),
-        /*
-        body: ListView(children: [
-          Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                const SizedBox(height: 50),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                  child: Text(
-                    "Qual é a sua dívida?",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: TextFormField(
-                    controller: localDividaController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Campo obrigatório";
-                      }
-
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: '',
-                      filled: true,
-                      fillColor: Colors.white,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                  child: Text(
-                    "Qual é o valor?",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: TextFormField(
-                  controller: valorController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Campo obrigatório";
-                    }
-
-                    return null;
-                  },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: '',
-                      filled: true,
-                      fillColor: Colors.white,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                  child: Text(
-                    "Qual é a data de \nvencimento?",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: TextFormField(
-                    controller: dataController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Campo obrigatório";
-                      }
-
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: '',
-                      filled: true,
-                      fillColor: Colors.white,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 24),
-                ElevatedButton(
-                  onPressed: onPressed,
-                  /*
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return const HomePage();
-                        },
-                      ),
-                    );
-                  },
-                  */
-                  child: const Text(
-                    'Salvar',
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                    ),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Color(0xFF1A422B),
-                  ),
-                )
-              ]),
-        ]));
-    */
+      ),
+    );
   }
 
   buildBody() {
@@ -161,7 +38,8 @@ class _QuartaPageState extends State<QuartaPage> {
         Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: const <Widget>[
-            SizedBox(height: 50),
+
+            const SizedBox(height: 50),
             
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
@@ -194,7 +72,91 @@ class _QuartaPageState extends State<QuartaPage> {
                 ),
               ),
             ),
-            
+
+            const SizedBox(height: 16),
+
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              child: Text(
+                "Qual é o valor?",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: TextFormField(
+                controller: valorController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Campo obrigatório";
+                  }
+
+                  return null;
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '',
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 16),
+
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              child: Text(
+                "Qual é a data de \nvencimento?",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: TextFormField(
+                controller: dataController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Campo obrigatório";
+                  }
+
+                  return null;
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '',
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 24),
+
+            ElevatedButton(
+              onPressed: onPressed,
+              child: const Text(
+                'Salvar',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFF1A422B),
+              ),
+            ),
           ]
         ),
       ],
